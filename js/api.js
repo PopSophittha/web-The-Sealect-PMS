4edd3bcc2e5e0447bd28a31c5297f8a5be512ef5
PopSophittha <sophittha.b@gmail.com>
2026-04-25 15:32:12 +0700

    Update API_URL to new endpoint

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzWR9PT0Sc4yyBjVxmj13tIbCKsoSILLZyTb7Z0eSYd-mLP9H_ubkAcZMTT2oO3L_PU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyKRSbCLvhgN08FEPx1tu7Z1i53PvEz1FLtu--eKqAqlFaA2_9XIWTetr2bv54v5Yu6/exec";
 
 async function apiGet(action, params = {}) {
   const url = new URL(API_URL);
